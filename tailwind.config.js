f4eee06962c323796cdb359bd606edda3a20be6e
ayberkenis <ayberkenis@gmail.com>
2024-08-10 11:43:30 +0300

    Updated Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		colors: {
+			primary: "#084ec5", // BiLira Primary/Accent Color
+			neutral: "#23272e", // BiLira Neutral Color
+		},
 		extend: {},
 	},
 	plugins: [],
