70d88da54612b583f832974807384db0c4b590cd
ayberkenis <ayberkenis@gmail.com>
2024-08-13 04:21:51 +0300

    noopener noreferrer fix

--- src/layouts/home.tsx
+++ src/layouts/home.tsx
@@ -4,7 +4,12 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<div className="home">
 			{children}
-			<a href="https://github.com/ayberkenis" target="_blank" className="text-xs text-zinc-400 tracking-wider my-20">
+			<a
+				href="https://github.com/ayberkenis"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="text-xs text-zinc-400 tracking-wider my-20"
+			>
 				presented by ayberkenis
 			</a>
 		</div>
